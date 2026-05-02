5524ae4648f7de277ee0ab1d51feb86210f0823d
eboe62 <eboe62@hotmail.com>
2026-05-02 23:55:31 +0200

    correcciones docker-entrypoint

--- ops/stacks/cron/docker-entrypoint.sh
+++ ops/stacks/cron/docker-entrypoint.sh
@@ -5,8 +5,12 @@ CRONFILE="/opt/monitoring/ops/stacks/cron/monitoring.cron"
 LOG_DIR="/opt/monitoring/logs"
 
 # Aseguramos logs en runtime, SOLO crear logs si no existen (sin romper runtime)
+# - NO usar chown (no somos root)
+# - Crear solo si no existe
+# - No fallar nunca (CI safe)
+
 if [ ! -d "$LOG_DIR" ]; then
-    mkdir -p "$LOG_DIR"
+    mkdir -p "$LOG_DIR" || true
 fi
 
 # Exec manual
